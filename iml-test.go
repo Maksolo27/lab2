3c916d139208a49dedb51fdf5e4eef6d2ea39a7f
maxim <makkov2705@gmail.com>
2022-05-18 14:10:13 +0300

    (feature) Add test

--- iml-test.go
+++ iml-test.go
@@ -1,1 +1,38 @@
 package lab2
+
+import (
+	"fmt"
+	. "gopkg.in/check.v1"
+	"testing"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func (s *TestSuite) TestPrefixToInfix(c *C) {
+	examples := map[string]string{
+		"+ 5 * - 4 2 3": "3 2 4 - * 5 +",
+		"+ 2 3":         "3 2 +",
+	}
+
+	for prefix, infix := range examples {
+		res, err := PrefixToPostfix(prefix)
+		if err != nil {
+			c.Assert(err, ErrorMatches, infix)
+		} else {
+			c.Assert(res, Equals, infix)
+		}
+	}
+}
+
+func ExamplePrefixToPostfix() {
+	res, err := PrefixToPostfix("+ 2 3")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println(res)
+	}
+}
